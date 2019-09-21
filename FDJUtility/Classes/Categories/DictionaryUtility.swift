9775bd1db88b9fce1fe26358ed710317b70468cb
Joe <likethephoenix@163.com>
2019-09-21 23:37:54 +0800

    暴露方法

--- FDJUtility/Classes/Categories/DictionaryUtility.swift
+++ FDJUtility/Classes/Categories/DictionaryUtility.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension Dictionary where Key==String, Value==Any  {
+public extension Dictionary where Key==String, Value==Any  {
     
     func string(for key:String) -> String? {
         
